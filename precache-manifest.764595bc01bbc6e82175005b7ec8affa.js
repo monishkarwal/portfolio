6a7c8eb3d3e213f954f09a83913e519b70ca33ec
Monish Karwal <monishkarwal@MacBook-Pro.local>
2020-09-01 23:37:43 +0530

    Updates

---
+++ precache-manifest.764595bc01bbc6e82175005b7ec8affa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a639d3fd431b351afdb9e9ca638584e6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "29ac05d322652d76d177",
+    "url": "/portfolio/static/css/main.fc638f44.chunk.css"
+  },
+  {
+    "revision": "f08f67e567a94bdda0e4",
+    "url": "/portfolio/static/js/2.4e409578.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/portfolio/static/js/2.4e409578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ac05d322652d76d177",
+    "url": "/portfolio/static/js/main.e0eb5d85.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
